f6a259a6fc32b4e169eb5d78035fb8d2fa1ce270
amir_g <info@ghani.ch>
2020-03-27 16:34:05 +0100

    add email functionality

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt import views as jwt_views
 
+from app import settings
 
 urlpatterns = [
     path('', admin.site.urls),
@@ -28,3 +30,7 @@ urlpatterns = [
     path('backend/api/social/', include('app.social.urls')),
     path('backend/api/users/', include('app.users.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
